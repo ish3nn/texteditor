09be200cfa032bed8c58a80c69802e7594fb3fcb
ish3nn <themaxyr@gmail.com>
2026-02-11 15:31:20 +0300

    refine

--- config.h
+++ config.h
@@ -5,16 +5,26 @@
 #ifndef TEXTEDITOR_CONFIG_H
 #define TEXTEDITOR_CONFIG_H
 
-#define SIDEBAR_Color SDL_SetRenderDrawColor(context->render, 0, 0, 0, 255);
+#define SidebarRatio 0.15f
+#define SidebarColor 100, 100, 100, 255
+
+#define ButtonSize 1.25f
+
+#define GutterRatio 0.05f
+#define GutterColor 35, 35, 35, 255
+
+#define EditorColor 28, 28, 28, 255
+
+#define SIDEBAR_Color SDL_SetRenderDrawColor(context->render, 100, 100, 100, 255);
 #define GUTTER_Color SDL_SetRenderDrawColor(context->render, 35, 35, 35, 255);
 #define EDITOR_Color SDL_SetRenderDrawColor(context->render, 28, 28, 28, 255);
-#define SYMBOL "↩"
-#define WRAP_symbol SDL_Color color = {160,160,160,255};
+#define SYMBOL 0x21A9
+#define SYMBOL_Color SDL_Color color = {160,160,160,255};
 #define NUM_Color SDL_Color color = {120,120,120,255};
 
 #define FONT "InterVariable.ttf"
 
-#define FONT_Color SDL_Color color = {255,255,255,255};
+#define FONT_Color SDL_Color color = {120,120,120,255};
 #define FONT_EDITOR TTF_OpenFont(FONT, 48);
 #define FONT_GUTTER TTF_OpenFont(FONT, 38);
 
